5d86109895c399101e35e987a4bd640186e42a41
Mikko Tuominen <mikko.i.tuominen@gmail.com>
2025-04-24 10:54:28 +0300

    Changed the env configuration

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -12,9 +12,14 @@ app.use(express.json());
 
 // Connect to Mongo
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    retryWrites: true,
+    w: 'majority',
+    appName: 'Cluster0'
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('Mongo error:', err));
 
 // Mount routes
 app.use('/api/telemetry', telemetryRouter);
--- packages/backend/src/routes/telemetry.js
+++ packages/backend/src/routes/telemetry.js
@@ -8,7 +8,6 @@ router.post('/data', async (req, res) => {
   const { timestamp, temperature, humidity, light, water } = req.body;
 
   try {
-    // Save data
     const newReading = new Reading({
       timestamp: new Date(timestamp),
       temperature,
@@ -20,7 +19,6 @@ router.post('/data', async (req, res) => {
 
     console.log(`Telemertry saved at ${timestamp}: Temp=${temperature}, Hum=${humidity}, Light=${light}, Water=${water}`);
 
-    // Check for config updates
     const config = await Config.findOne().sort({ updatedAt: -1 });
     const needsUpdate = config ? config.updatedAt > Date.now() - 60000 : false;
 
