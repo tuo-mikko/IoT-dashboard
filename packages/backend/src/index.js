e5ba20375c39383603849f9fa1e1d60907fc5685
Mikko Tuominen <mikko.i.tuominen@gmail.com>
2025-04-24 16:07:11 +0300

    Heroku frontend

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -39,10 +39,9 @@ app.use('/api/config',     configRouter);
 const path = require('path');
 
 const buildPath = path.join(__dirname, '..', '..', 'frontend', 'build');
-// serve the React build under “/”
+
 app.use('/', express.static(buildPath));
 
-// SPA fallback for React Router
 app.get(/^\/(?!api).*/, (_req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
 });
